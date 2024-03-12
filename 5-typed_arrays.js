e24346457a1470f7cd4e9cc8b4b948249a23650d
SanaeMN <Sanaeenemuom2017@gmail.com>
2024-03-12 16:58:33 -0700

    5. Typed Arrays

---
+++ 5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0, length);
+  view.setInt8(position, value);
+
+  return view;
+}
