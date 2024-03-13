933457b7c0ec7954445dad32b7a786ef52c8b65f
SanaeMN <Sanaeenemuom2017@gmail.com>
2024-03-12 17:19:45 -0700

    remove file

--- 2-get_students_by_loc.js
+++
@@ -1,4 +0,0 @@
-export default function getStudentsByLocation(ListStudents, city) {
-  if (!Array.isArray(ListStudents)) return [];
-  return ListStudents.filter((student) => student.location === city);
-}
