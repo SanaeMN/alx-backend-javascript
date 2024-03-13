4746a4dab76ffe523dcb7632463bdb49694c652b
SanaeMN <Sanaeenemuom2017@gmail.com>
2024-03-12 17:20:25 -0700

    remove file

--- 3-get_ids_sum.js
+++
@@ -1,7 +0,0 @@
-export default function getStudentIdsSum(ListStudents) {
-  if (!Array.isArray(ListStudents)) return 0;
-  return ListStudents.map((student) => student.id).reduce(
-    (prev, next) => prev + next,
-    0,
-  );
-}
